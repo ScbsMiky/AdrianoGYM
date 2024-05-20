c21df2e58da7945cdf3cf3f9e761e1974f3a7c1b
Miky <66943563+ScbsMiky@users.noreply.github.com>
2024-05-20 05:51:35 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ export default function App( ) {
     clients.fetchCustumers( )
         .then((custumers) => {
           setMyClients(clients);
+          setIsLoading(false);
         });
   }, [ ]);
   
